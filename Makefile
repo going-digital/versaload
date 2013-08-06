8af773804d48e8af116f8112cbde29495d47b878
Peter Knight <cathedrow@me.com>
2013-08-07 00:04:30 +0100

    Fix build issues. Now compiles to Z80 binary.

--- Makefile
+++ Makefile
@@ -3,18 +3,20 @@ TZXLIST=./tzxlist
 PYTHON=python
 FUSE=/Applications/Fuse.app/Contents/MacOS/Fuse
 
-%.bin: %.pasmo
-    $(PASMO) --bin $< $@ gl
+%.bin: %.asm
+	$(PASMO) --bin $< $@ gl
 
 boot1.bin: boot2.bin
 
-test.tzx: boot1.bin
-    $(PYTHON) buildtzx.py
-    $(TZXLIST) test.tzx
+#test.tzx: boot1.bin
+#    $(PYTHON) buildtzx.py
+#    $(TZXLIST) test.tzx
 
-fuse: versaload.tzx
-    $(FUSE) --machine plus2 --no-detect-loader --tape versaload.tzx
+#fuse: versaload.tzx
+#    $(FUSE) --machine plus2 --no-detect-loader --tape versaload.tzx
+
+all: boot1.bin
 
 clean:
-    rm -f *.bin
-    rm -f *.tzx
+	rm -f *.bin
+	rm -f *.tzx
--- boot2.asm
+++ boot2.asm
@@ -2,6 +2,9 @@
 
 ; TODO: Get this code complete and tested
 
+        include "globals.inc"
+        include "undoc_instr.inc"
+
         org loadbase
 
         ; Sync:
@@ -21,19 +24,19 @@ sync_loop:
         rl      h                       ; 8T
         ld      a,sync_train_1          ; 7T
         cp      h                       ; 4T
-        ld      a,(69+15)/29            ; 7T
+        ld      a,+(69+15)/29           ; 7T
         jr      nz,sync_loop            ; 12T/7T exit: 69T
         ld      a,sync_train_2          ; 7T
         cp      l                       ; 4T
-        ld      a,(94+15)/29            ; 7T
+        ld      a,+(94+15)/29           ; 7T
         jr      nz,sync_loop            ; 12T/7T exit: 94T
         ld      a,sync_train_3          ; 7T
         cp      d                       ; 4T
-        ld      a,(119+15)/29           ; 7T
+        ld      a,+(119+15)/29          ; 7T
         jr      nz,sync_loop            ; 12T/7T exit: 119T
         ld      a,sync_train_4          ; 7T
         cp      e                       ; 4T
-        ld      a,(144+15)/29           ; 7T
+        ld      a,+(144+15)/29          ; 7T
         jr      nz,sync_loop            ; 12T/7T exit: 144T
         ; Sync has been received, 139T after edge so far
 
@@ -45,7 +48,7 @@ sync_loop:
         ; as well as tape stretch, which gives much more accurate
         ; descrimination between symbols.
 calibrate:
-        ld      hl,(161+19+19+19+15)/29 ; 10T
+        ld      hl,+(161+19+19+19+15)/29; 10T
         ld      d,h                     ; 4T
         ld      e,l                     ; 4T
         xor     a                       ; 4T = 161T total
@@ -82,14 +85,14 @@ calibrate:
         ; Done in Gray code order for fastest calculation speed.
         ld      a,b                     ; 4T
         add     a,c                     ; 4T
-        ld      (thres_1_5),a           ; 13T
+        ;ld      (thres_1_5),a           ; 13T
         add     a,d                     ; 4T
         ld      (thres_3_5),a           ; 13T
-        sub     a,c                     ; 4T
+        sub     c                       ; 4T
         ld      (thres_2_5),a           ; 13T
         add     a,e                     ; 4T
         ld      (thres_6_5),a           ; 13T
-        sub     a,d                     ; 4T
+        sub     d                       ; 4T
         ld      (thres_4_5),a           ; 13T
         add     a,c                     ; 4T
         ld      (thres_5_5),a           ; 13T
@@ -118,65 +121,70 @@ calibrate:
         ; must complete within 700T otherwise the next pulse measurement may
         ; be compromised.
 
-        ld      a,NN            ; 7T
+thres_2_5 equ smc02+1
+thres_3_5 equ smc01+1
+thres_4_5 equ smc04+1
+thres_5_5 equ smc03+1
+thres_6_5 equ smc07+1
+thres_7_5 equ smc06+1
+thres_8_5 equ smc08+1
+
+; TODO: Recalculate timings through this section
+
+        ld      a,0             ; 7T
 readdata:
         call    measure_symbol
-smc01:  cp      0               ; 7T
-thres_3_5 equ smc01+1
+smc01:  cp      0               ; 7T    thres_3_5 stored here
         jp      c,bits_1_       ; 10T
         call    addbit
-smc02:  cp      0               ; 7T
-thres_2_5 equ smc02+1
+smc02:  cp      0               ; 7T    thres_2_5 stored here
         call    addbit
-        ld      a,(108+15)/29   ; 7T
-        jp      readdataend     ; 10T [111T since edge]
+        ld      a,+(108+15)/29  ; 7T
+        jp      readdataend     ; 10T
 
 bits_1_:call    addbit
-smc03:  cp      0               ; 7T
-thres_5_5 equ smc03+1
+smc03:  cp      0               ; 7T    thres_5_5 stored here
         jp      c,bits_11_      ; 10T        [10 + 54]
         call    addbit
-smc04:  cp      0               ; 7T
-thres_4_5 equ smc04+1
+smc04:  cp      0               ; 7T    thres_4_5 stored here
         call    addbit
-        ld      a,(159+15)/29   ; 7T
-        jp      readdataend     ; 10T   [158T since edge]
+        ld      a,+(159+15)/29  ; 7T
+        jp      readdataend     ; 10T
 
-bits_11_: ;[64T]
+bits_11_:
         call    addbit
-smc06:  cp      0               ; 7T
-thres_7_5 equ smc06+1
+smc06:  cp      0               ; 7T    thres_7_5 stored here
         jr      c,bits_111_     ; 12T/7T
         call    addbit
-smc07:  cp      0               ; 7T
-thres_6_5 equ smc07+1
+smc07:  cp      0               ; 7T    thres_6_5 stored here
         call    addbit
-ret08:  ld      a,(213+15)/29   ; 7T
-        jp      readdataend     ; 10T [207T] **TOO*LONG**
+ret08:  ld      a,+(213+15)/29  ; 7T
+        jp      readdataend     ; 10T
 
-bits_111_:;[118T]
+bits_111_:
         call    addbit
-smc08:  cp      0               ; 7T
-thres_8_5 equ smc08+1
+smc08:  cp      0               ; 7T    thres_8_5 stored here
         call    addbit
-        ld      a,(206+15)/29   ; 7T [202T] **TOO*LONG**
+        ld      a,+(206+15)/29  ; 7T
+
 readdataend:
         ;
         ; TODO: Handling of binary datastream goes here
         ;
         ; TODO: Count bytes, then jump to either readdata or sync
         ;
-
-smc99:  ;ld      a,NN            ; 7T
-        ;ld      (smc99+1),d     ;
-        ;cp      d
-        ; If the shift register is smaller than last time, a byte has been written
-        ;jp      nc,?
-        ;inc     hl
-        cp      h,b
-        jp      nz,?
-        cp      l,c
-        jp      nz,?
+        ; TODO: Need to test for end-of-block condition
+        ;
+        ld      e,a
+        ld      a,h
+        cp      b
+        ld      a,e
+        jp      nz,readdata
+        ld      a,l
+        cp      c
+        ld      a,e
+        jp      nz,readdata
+        ; HL = BC, end of block condition so resync
         jp      sync
 
 
@@ -193,7 +201,7 @@ alertlp:out     (c),a
         jr      alertlp
 
 addbit: rl      d
-        jr      nc,del10
+        jr      nc,delab
         ld      (hl),d
         ld      d,$01
         inc     hl
@@ -218,15 +226,17 @@ measure_symbol:
 measure_half_symbol:
         exx
         ld      c,$FE           ; Mic port
+        ex      af,af'
+        ld      a,ms_0 - ms_1
+        ld      (ms_link+1),a
+        ex      af,af'
 ms_link:jr      ms_1            ; * Selfmodifying code
-ms_1:   ld      b,ms_0 - ms_1
-        ld      ms_link + 1,b
+ms_1:   
 ms1lp:  inc     a               ;  7T
         IN_F_C                  ; 12T in f,(c) undocumented instruction
         jp      po,ms1lp        ; 10T
         ret
-ms_0:   ld      b,ms_0 - ms_0
-        ld      ms_link + 1,b
+ms_0:   
 ms0lp:  inc     a
         IN_F_C
         jp      pe,ms0lp
