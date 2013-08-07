ee13c3aef6dae5ec69d9207087f309155db69d34
Peter Knight <cathedrow@me.com>
2013-08-07 14:37:39 +0100

    Loading speed improvement tweak in bootstrap

--- buildtzx.py
+++ buildtzx.py
@@ -14,11 +14,20 @@ loaderheader = ZX_FileHdr(SPEC_FILE_PROG, 'Versaload', 0, 1, 0)
 loaderdata = ZX_FileData(loaderprog)
 loaderheader.setdatalen(loaderdata.datalen())
 loaderblock1 = Blk_TSDB(data=loaderheader.get())
-loaderblock1.bitpulse0(650)
-loaderblock1.bitpulse1(1710)
+#
+# Set to absolute fastest settings then back off by 10%
+# This achieves 2176 baud instead of 1535 baud with the stock ROM loader.
+#
+loaderblock1.bitpulse0(557)   # Works 506
+loaderblock1.bitpulse1(1445)  # Works 1314
+loaderblock1.pilotpulse(1949) # Works 1772
+loaderblock1.pilottone(4426)  # Works 4024
+loaderblock1.pause(0)         # Works 0
 loaderblock2 = Blk_TSDB(data=loaderdata.get())
-loaderblock2.bitpulse0(650)
-loaderblock2.bitpulse1(1710)
+loaderblock2.bitpulse0(557)
+loaderblock2.bitpulse1(1445)
+loaderblock2.pilotpulse(1949)
+loaderblock2.pilottone(2564)  # Works 2331
 loaderblock2.pause(10)
 
 """
