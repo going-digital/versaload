edd631ba9344ba172f637fb22889e3b3f350c550
Peter Knight <cathedrow@me.com>
2013-09-10 10:59:39 +0100

    Correct delay calculation

--- versaload.py
+++ versaload.py
@@ -1,4 +1,5 @@
 import struct
+import math
 from bitstring import BitArray
 
 class Bmc:
@@ -49,11 +50,15 @@ class Versaload:
         self.__bitstream.append('0b0011111111111101')
     def delay(self, time):
         print "Wait ",time
-        self.__bitstream.append('0b1'*int(round((self.__baud*time))))
+        self.__bitstream.append('0b1'*int(math.ceil((self.__baud*time))))
     def load(self, address, data):
         while len(data) > 0:
+            #
+            # Split into 256 byte blocks
+            #
             print hex(self.__blockNumber),"Load",hex(address)
             dataBlock = data[:0x100]
+            #
             # Build header
             #
             length = len(dataBlock) % 256
@@ -62,49 +67,67 @@ class Versaload:
                 dataChecksum = (dataChecksum + ord(byte)) % 256
             dataChecksum = (256 - dataChecksum) % 256
             header = struct.pack('<HH',self.__blockNumber,address)
-            header = header + struct.pack('<BBB',length,0,dataChecksum)
+            header += struct.pack('<BBB',length,0,dataChecksum)
             headerChecksum = 0
             for byte in list(header):
                 headerChecksum = (headerChecksum + ord(byte)) % 256
             headerChecksum = (256 - headerChecksum) % 256
-            header = header + struct.pack('<B',headerChecksum)
-
-            # Add data
+            header += struct.pack('<B',headerChecksum)
+            #
+            # Generate bitstream
             #
             self.__addheader()
             self.__bitstream.append(BitArray(bytes=header))
             self.__bitstream.append(BitArray(bytes=dataBlock))
+            #
             # Prepare for next block
-            self.__blockNumber = self.__blockNumber + 1
+            #
+            self.__blockNumber += 1
             data = data[0x100:]
             address = address + 0x100
         self.__bitstream.append('0xff')
 
     def execute(self, address, execTime):
         print hex(self.__blockNumber),"Exec",hex(address)
+        #
         # Build header
         #
         header = struct.pack('<HH',self.__blockNumber, address)
-        header = header + struct.pack('<BBB',0,1,0)
+        header += struct.pack('<BBB',0,1,0)
         headerChecksum = 0
         for byte in list(header):
             headerChecksum = (headerChecksum + ord(byte)) % 256
         headerChecksum = (256 - headerChecksum) % 256
-        header = header + struct.pack('<B',headerChecksum)
-        
+        header += struct.pack('<B',headerChecksum)
+        #
         # Generate bitstream
+        #
         self.__addheader()
         self.__bitstream.append(BitArray(bytes=header))
         self.__bitstream.append('0xff')
+        #
+        # Add pause to resync after execution
+        #
         self.delay(execTime)
+        #
         # Prepare for next block
-        self.__blockNumber = self.__blockNumber + 1
+        #
+        self.__blockNumber += 1
 
     def tStatesPerSample(self):
+        #
+        # For TZX format, assumes 3.5MHz Z80 clock rate
+        #
+        # Note this is very Sinclair-centric.
+        # Amstrad: 4.0MHz Z80 clock rate
+        # Commodore: 2.0MHz 6502 clock rate
+        #
         return int(round(0.5*3500000/self.__baud))
 
     def get(self):
+        #
         # Modulate bitstream before returning it
+        #
         modulated = Bmc()
         modulated.add(self.__bitstream)
         return modulated.get()
