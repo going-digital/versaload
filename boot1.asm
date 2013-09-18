017045fce4c9eb8224b4a8a50ae14ed16c96e087
Peter Knight <cathedrow@me.com>
2013-09-18 10:25:50 +0100

    Fix incompatibility with Interface 1
    BASIC loader no longer assumes PROG = $5ccb.

--- boot1.asm
+++ boot1.asm
@@ -1,36 +1,54 @@
         include "tokens.inc"
         include "globals.inc"
         include "romhooks.inc"
+        include "sysvar.inc"
 
 b_num   macro   n
-        db      '0'
-        db      $0e
-        db      0
-        db      0
-        db      low(n)
-        db      high(n)
-        db      0
+        db      '0',$0e,0,0,low(n),high(n),0
         endm
 
-
-        org     $5ccb           ; Base address for all Spectrum BASICs
+        org     $5ccb           ; Base address for most Spectrum BASICs
 
 ; BASIC bootstrap to machine code
-lineno  equ     1
-        db      high(lineno),low(lineno)        ; Line number is big endian!
+lineno  equ     0
+basic:  db      high(lineno),low(lineno)        ; Line number is big endian!
         dw      lineend-linestart               ; Line length is little endian
 linestart:
-        db      T_RANDOMIZE     ; In BASIC, this appears as:
-        db      T_USR           ;   <line no> OVER USR 7<garbage>
-        b_num   code
+        db      T_CLS,':',T_RANDOMIZE,T_USR
+
+        ; 0 CLS:RANDOMIZE USR (xx+PEEK 23635+256*PEEK 23636)
+        ; although it appears as
+        ; 0 CLS:RANDOMIZE USR (0+PEEK 0+0*PEEK 0)
+        ; to keep the size small.
+
+;        ; 28 bytes longer, but copes with different PROG bases
+        db      '('
+        b_num   code-basic
+        db      '+',T_PEEK
+        b_num   PROG
+        db      '+'
+        b_num   $100
+        db      '*',T_PEEK
+        b_num   PROG+1
+        db      ')'
+
+        ; 0 RANDOMIZE USR n
+        ; This version is smaller, but assumes PROG = $5ccb
+        ; which is true for most machines, but not Timex's or those
+        ; expanded by peripherals like Interface 1 or disk drives.
+;        b_num   code
+
         db      $0d
+
 lineend:
 
 ; Machine code section
 code:
         di
         ld      de,loadbase
-        ld      hl,reloc_start
+        ld      hl,reloc_start - code   
+        add     hl,bc           ; On entry, BC = code
+
         push    de              ; Set execution address of loader
         ld      bc,reloc_end - reloc_start
         ldir                    ; Relocate loader
